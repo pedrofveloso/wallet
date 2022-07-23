3537f01c3c9838e95d42b0ef5d688a5696f43d95
Pedro Veloso <pedrofveloso@gmail.com>
2022-07-23 03:01:47 -0300

    create project

---
+++ Wallet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wallet
+//
+//  Created by Pedro Veloso on 23/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
