b538b4aaf31f5b54ea55ace2fe61344d0629efdb
Pedro Veloso <pedrofveloso@gmail.com>
2022-07-24 19:42:46 -0300

    add statement table view component; organize folders

---
+++ Wallet/Source/Statement/Components/StatementTableView.swift
@@ -0,0 +1,52 @@
+//
+//  StatementTableView.swift
+//  Wallet
+//
+//  Created by Pedro Veloso on 24/07/22.
+//
+
+import UIKit
+
+typealias StatementTableViewProtocol = UITableViewDataSource & UITableViewDelegate
+
+class StatementTableView: UIView {
+    private let cellID = "statementCellIdentifier"
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        
+        tableView.dataSource = parent
+        tableView.delegate = parent
+        
+        return tableView
+    }()
+    
+    private weak var parent: StatementTableViewProtocol?
+    
+    init(parent: StatementTableViewProtocol, frame: CGRect = .zero) {
+        super.init(frame: frame)
+        self.parent = parent
+        buildViewCode()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension StatementTableView: ViewCodable {
+    func buildHierarchy() {
+        addSubview(tableView)
+    }
+    
+    func buildConstraints() {
+        tableView
+            .edges(to: self)
+    }
+    
+    func buildAdditionalConfigurations() {
+        backgroundColor = .systemBackground
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
