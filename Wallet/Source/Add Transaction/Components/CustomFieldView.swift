aa77ff8d2376ef8aced436a7aeec2d3e8cb2e116
Pedro Veloso <pedrofveloso@gmail.com>
2022-07-27 00:50:28 -0300

    add custom field view component

---
+++ Wallet/Source/Add Transaction/Components/CustomFieldView.swift
@@ -0,0 +1,145 @@
+//
+//  CustomFieldView.swift
+//  Wallet
+//
+//  Created by Pedro Veloso on 25/07/22.
+//
+
+import UIKit
+
+protocol CustomFieldView: UITextFieldDelegate {
+    func didSelectUp(selector: CustomTextFieldView)
+    func didSelectDown(selector: CustomTextFieldView)
+}
+
+final class CustomTextFieldView: UITextField {
+    // MARK: - Properties
+    weak var customDelegate: CustomFieldView? {
+        didSet {
+            delegate = customDelegate
+        }
+    }
+    
+    var textEditingEnabled = true
+    
+    private let selector: SelectorType
+    private let textInsets = UIEdgeInsets(top: 0.0, left: 8.0, bottom: 0.0, right: 12.0)
+    
+    // MARK: - Overriden methods
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        let rect = super.textRect(forBounds: bounds)
+        return rect.inset(by: textInsets)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        let rect = super.editingRect(forBounds: bounds)
+        return rect.inset(by: textInsets)
+    }
+    
+    override func rightViewRect(forBounds bounds: CGRect) -> CGRect {
+        let rect = super.rightViewRect(forBounds: bounds)
+        return rect.inset(by: .init(top: 0.0, left: -8.0, bottom: 0.0, right: 8.0))
+    }
+    
+    override func leftViewRect(forBounds bounds: CGRect) -> CGRect {
+        let rect = super.leftViewRect(forBounds: bounds)
+        return rect.inset(by: .init(top: 0.0, left: 8.0, bottom: 0.0, right: -8.0))
+    }
+    
+    override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        textEditingEnabled
+    }
+
+    override func selectionRects(for range: UITextRange) -> [UITextSelectionRect] {
+        textEditingEnabled ? super.selectionRects(for: range) : []
+    }
+
+    override func caretRect(for position: UITextPosition) -> CGRect {
+        textEditingEnabled ? super.caretRect(for: position) : .null
+    }
+    
+    // MARK: - Inits
+    init(selector: SelectorType, frame: CGRect = .zero) {
+        self.selector = selector
+        
+        super.init(frame: .zero)
+        
+        buildViewCode()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - View codable methods
+extension CustomTextFieldView: ViewCodable {
+    func buildHierarchy() {}
+    
+    func buildConstraints() {
+        height(48.0)
+    }
+    
+    func buildAdditionalConfigurations() {
+        translatesAutoresizingMaskIntoConstraints = false
+        addCustomBorder(radius: 8.0)
+        
+        buildSelectorIfNeeded()
+    }
+}
+
+// MARK: - Private methods
+private extension CustomTextFieldView {
+    func buildSelectorIfNeeded() {
+        switch selector {
+        case .single:
+            rightView = downSelector
+            rightViewMode = .always
+            
+        case .double:
+            let vStack = UIStackView(arrangedSubviews: [upSelector, downSelector])
+            vStack.axis = .vertical
+            vStack.distribution = .fillEqually
+
+            rightView = vStack
+            rightViewMode = .always
+        
+        case .none:
+            break
+        }
+    }
+    
+    var upSelector: UIButton {
+        let button = UIButton()
+        button.setImage(.init(systemName: "chevron.up"), for: .normal)
+        button.addTarget(self, action: #selector(didSelectUp), for: .touchUpInside)
+        
+        return button
+    }
+    
+    var downSelector: UIButton {
+        let button = UIButton()
+        button.setImage(.init(systemName: "chevron.down"), for: .normal)
+        button.addTarget(self, action: #selector(didSelectDown), for: .touchUpInside)
+        
+        return button
+    }
+    
+    @objc
+    func didSelectUp() {
+        customDelegate?.didSelectUp(selector: self)
+    }
+    
+    @objc
+    func didSelectDown() {
+        customDelegate?.didSelectDown(selector: self)
+    }
+}
+
+// MARK: - Selector type
+extension CustomTextFieldView {
+    enum SelectorType {
+        case none, single, double
+    }
+}
